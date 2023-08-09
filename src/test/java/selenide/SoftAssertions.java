9229a44eb4b18f4094f46b27bdf526f396501ef4
Overloque <poludnicyn.99@mail.ru>
2023-08-09 19:25:55 +0500

    Github Selenide check

---
+++ src/test/java/selenide/SoftAssertions.java
@@ -0,0 +1,38 @@
+package selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertions {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void SelenidePageTest() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link").$("[type=button]").click();
+        $(".wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(".wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("class Tests {\n" +
+                "  @RegisterExtension \n" +
+                "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+                "\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
